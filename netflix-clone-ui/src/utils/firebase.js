f28ca95533807d38e0d42c307ac315f19d37657e
ParasNingune <153178176+ParasNingune@users.noreply.github.com>
2024-10-10 23:03:32 +0530

    Update firebase.js

--- netflix-clone-ui/src/utils/firebase.js
+++ netflix-clone-ui/src/utils/firebase.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAaXf5QrSzwQo01QLiQXpHTyl2_lRdsQcA",
-  authDomain: "netflix-clone-react-8b927.firebaseapp.com",
-  projectId: "netflix-clone-react-8b927",
-  storageBucket: "netflix-clone-react-8b927.appspot.com",
-  messagingSenderId: "494376152443",
-  appId: "1:494376152443:web:72ea37d4f5a098a65d21de",
-  measurementId: "G-2DVLTHER0P"
+Paste your firebase Config here
 };
 
 
